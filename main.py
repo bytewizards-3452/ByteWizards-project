3a0115ec25d3dbdbfaa2f041610395e00b7438cd
bytewizards-3452 <bytewizards3452@gmail.com>
2025-08-13 23:02:22 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
 import os
+print("CWD:", os.getcwd())
+print("Files in CWD:", os.listdir("."))
 
 app = FastAPI()
 
